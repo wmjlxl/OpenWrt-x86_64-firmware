1b804527058e09b943e9f1f03eb91bd0c8f24bb3
wmjlxl <wmjlxl@gmail.com>
2021-06-02 14:43:56 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,4 +18,5 @@
 #sed -i '$a src-git luci-app-adguardhome https://github.com/rufengsuixing/luci-app-adguardhome' feeds.conf.default
 #
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 #
